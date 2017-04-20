154d76a7cb4e61df34d85fe8aaf29892e5086c81
Justin Sprenger <justin_sprenger@hotmail.de>
2017-04-20 11:33:28 +0200

    unwichtige veränderung

--- src/database/Test.java
+++ src/database/Test.java
@@ -5,10 +5,15 @@ import database.Database;
 public class Test {
 
 	public static void main(String[] args) throws ClassNotFoundException {
+		/**
 		Database db = new Database("localhost",3306,"test","root","");
 		String[] val = new String[]{"'Hans'","'Peter'","'2003'"};
 		System.out.println(db.connect());
 		db.insertDatabase("t_accounts", val);
+		**/
+		Database db = new Database("192.168.178.250",3306,"test","root","Tresor132");
+		String[] val = new String[]{"'Hans'","'Peter'","'2003'"};
+		System.out.println(db.connect());
 	}
 
 }
