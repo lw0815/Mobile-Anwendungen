082d670799b168c967267c079e5231af33283d77
Justin <justin_sprenger@hotmail.de>
2017-04-20 11:26:26 +0200

    messungen

--- src/database/Test.java
+++ src/database/Test.java
@@ -6,9 +6,9 @@ public class Test {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		Database db = new Database("localhost",3306,"test","root","");
-		String[] val = new String[]{"'Hans'","'Peter'","'2003'"};
+		String[] val = new String[]{"'192'","'10:33'","'18.04.2017'","'33000'","'Irgendwas'"};
 		System.out.println(db.connect());
-		db.insertDatabase("t_accounts", val);
+		db.insertDatabase("t_Messungen", val);
 	}
 
 }
